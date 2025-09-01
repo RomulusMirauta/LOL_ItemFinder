b62dc7b9a8a30cc0117775b870035ddf518410d7
RomulusMirauta <romulus.mirauta1@gmail.com>
2025-09-02 02:17:41 +0300

    add disclaimer about Riot Games endorsement to Filters component

--- lol-itemfinder/src/components/Filters.tsx
+++ lol-itemfinder/src/components/Filters.tsx
@@ -339,4 +339,9 @@ const Filters: React.FC<FiltersProps> = ({ filterState, setFilterState }) => {
   );
 };
 
+
+// TO BE ADDED: 
+// "League of Legends - Item Finder" is not endorsed by Riot Games and does not reflect the views or opinions of Riot Games or anyone officially involved in producing or managing Riot Games properties. Riot Games and all associated properties are trademarks or registered trademarks of Riot Games, Inc
+
+
 export default Filters;
