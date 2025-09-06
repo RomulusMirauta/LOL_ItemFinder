16690d4297fa0b442c394f64ce0979cae13d734a
RomulusMirauta <romulus.mirauta1@gmail.com>
2025-09-06 23:57:19 +0300

    refactor ItemCard component; implement cleaning functions for item names and plaintext, and update rendering logic for improved clarity

--- lol-itemfinder/src/components/ItemCard.tsx
+++ lol-itemfinder/src/components/ItemCard.tsx
@@ -2,6 +2,27 @@ import React from 'react';
 import type { ItemCardProps } from '../types/types';
 
 import '../styles/ItemCard.css';
+
+
+function cleanItemName(name: string): string {
+  return name
+    .replace(/<[^>]+>/g, '') // Remove all HTML-like tags
+    .replace(/@[\w]+@/g, '') // Remove Riot placeholders
+    .replace(/\s+/g, ' ')
+    .trim();
+}
+
+function cleanPlainText(plaintext: string): string {
+  return plaintext
+    .replace(/@[\w]+@/g, '')
+    .replace(/<status>(.*?)<\/status>/g, '$1')
+    .replace(/<magicDamage>(.*?)<\/magicDamage>/g, '$1')
+    .replace(/<recast>(.*?)<\/recast>/g, '$1')
+    .replace(/<[^>]+>/g, '')
+    .replace(/\s+/g, ' ')
+    .trim();
+}
+
 const ItemCard: React.FC<ItemCardProps> = ({ item, version, isActive, setActiveItem }) => (
   <div
     className={`item-card${isActive ? ' active' : ''}`}
@@ -18,15 +39,25 @@ const ItemCard: React.FC<ItemCardProps> = ({ item, version, isActive, setActiveI
       alt={item.name}
       className="item-card-img"
     />
-    <h3 className="item-card-title">{item.name}</h3>
+
+    {/* <h3 className="item-card-title">{item.name}</h3> */}
+    <h3 className="item-card-title">{cleanItemName(item.name ?? '')}</h3>
+
     <div className="item-card-gold-container">
       <span className="item-card-gold">{item.gold?.total ?? 0} Gold</span>
     </div>
-    <p className="item-card-plaintext">{item.plaintext}</p>
+    {/* <p className="item-card-plaintext">{item.plaintext}</p> */}
+    
+    <div className="item-card-plaintext"><p>
+      {/* {item.plaintext} */}
+      {cleanPlainText(item.plaintext ?? '')}
+      </p></div>
+
     {isActive && (
       <div className="item-card-description">
         <strong>Description:</strong>
         <div dangerouslySetInnerHTML={{ __html: item.description }} />
+        {/* <div>{cleanDescription(item.description)}</div> */}
       </div>
     )}
   </div>
